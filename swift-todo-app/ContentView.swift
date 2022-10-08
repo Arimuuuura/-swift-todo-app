f8acf482bb5190cc794038af0b300408fd8f487a
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-10-08 18:58:45 +0900

    Initial Commit

---
+++ swift-todo-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-todo-app
+//
+//  Created by 有村優希 on 2022/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-todo-app/swift_todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todo_appApp.swift
+//  swift-todo-app
+//
+//  Created by 有村優希 on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct swift_todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
